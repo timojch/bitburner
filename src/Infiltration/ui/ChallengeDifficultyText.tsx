2aee03a5c1b37ba16a703aafb7db2a670ee2fd82
timojch <timo.ch@outlook.com>
2022-01-15 02:31:12 -0800

    Difficulty pass. Very rough.

---
+++ src/Infiltration/ui/ChallengeDifficultyText.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+import { Theme } from "@mui/material/styles";
+import makeStyles from "@mui/styles/makeStyles";
+import createStyles from "@mui/styles/createStyles";
+
+import Typography from "@mui/material/Typography";
+
+type IProps = {
+  difficulty: number
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    access: {
+      color: theme.colors.hack,
+    },
+    healing: {
+      color: theme.colors.hp,
+    },
+    reduceAlarm: {
+      color: theme.colors.white,
+    },
+    escape: {
+      color: theme.colors.cha
+    }
+  }),
+);
+
+const difficultyText : [number, string][] = [
+  
+  [3.5, "Impossible"],
+  [3.0, "Almost Impossible"],
+  [2.5, "Very Difficult"],
+  [2.0, "Difficult"],
+  [1.5, "Challenging"],
+  [1.0, "Normal"],
+  [0.5, "Easy"],
+  [0.0, "Routine"],
+  
+]
+
+const difficultyColors : [number, string][] = [
+  [3.5, "purple"],
+  [3.0, "red"],
+  [2.5, "orange"],
+  [1.5, "yellow"],
+  [1.0, "lightgreen"],
+  [0.5, "white"],
+  [0.0, "gray"],
+]
+
+function lookup(value: number, table: [number, string][]) : string{
+  for(var pair of table) {
+    if(pair[0] < value) {
+      return pair[1];
+    }
+  }
+  return "invalid";
+}
+
+export function ChallengeDifficultyText(props: IProps): React.ReactElement {
+  var difficultyName = lookup(props.difficulty, difficultyText)
+  var difficultyColor = lookup(props.difficulty, difficultyColors);
+  return <Typography>Difficulty: <span style={{ color: difficultyColor }}>{difficultyName}</span></Typography>
+}
